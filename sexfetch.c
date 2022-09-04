d420e1417e99b8d053d806aa21d5c16e006d20fc
Komodo <loja@duck.com>
2022-09-04 13:40:00 +0200

    Update sexfetch.c

--- sexfetch.c
+++ sexfetch.c
@@ -6,8 +6,8 @@ void get_info(){
     struct utsname s;
     uname(&s);
     printf("System: %s, %s\n",s.sysname,s.version);
+    printf("Kernel: %s\nCpu Architecture: %s\n",s.release,s.machine);
     printf("Hostname: %s\n",s.nodename);
-    printf("Kernel: %s\nCpu: %s\n",s.release,s.machine);
 }
 
 int main(){
@@ -17,19 +17,19 @@ int main(){
                     " /',        |\n" "|   `-,_,__,'\n"
                     "(,,)====[_]=|\n" "  '.   ____/\n"
                     "   | -|-|_\n" "   |____)_)";
+
     printf("%s\n\n", banner);
-    system("cat /etc/issue");    //if linux distribution return the name
+    system("cat /etc/issue");    //if it's a Linux distribution, returns the name
     get_info();
     const char * sexium[]={
         "",                     
         getenv("USER"),        //get enviroment variables
         getenv("SHELL"),
     };
-    for (int i=0; i < 3; i++){     //i do this to avoid segmentation fault
+    for (int i=1; i < 3; i++){     
         if (i == 1){
-            printf("User: %s\n", sexium[i]);
-        }
-        if (i == 2){
+            printf("User: %s\n", sexium[i]);    //Spaghetti code time
+        }else{
             printf("Shell: %s\n\n", sexium[i]);
         }   
     }
