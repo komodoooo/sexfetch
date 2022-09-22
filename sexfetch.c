1361230063a19d2d10e0327b03b8aff4ff11e5e0
Komodo <loja@duck.com>
2022-09-22 22:11:36 +0200

    Update sexfetch.c

--- sexfetch.c
+++ sexfetch.c
@@ -3,38 +3,37 @@
 #include <sys/utsname.h>
 
 void get_info(){
+    system("cat /etc/issue");  //if it's a Linux distribution, returns the name
     struct utsname s;
     uname(&s);
     printf("System: %s, %s\n",s.sysname,s.version);
     printf("Kernel: %s\nCpu Architecture: %s\n",s.release,s.machine);
     printf("Hostname: %s\n",s.nodename);
-    system("echo Ram: $(vmstat | awk 'END {printf $4}') Kb");
+    system("echo Ram: $(vmstat | awk 'END {printf $4}') Kb"); //get the ram
+    const char * sexium[]={ 
+        "User",
+        "Home path",
+        "Shell"
+    };                                //dirty codes go! brrr
+    const char * envs[]={
+        getenv("USER"),
+        getenv("HOME"), 
+        getenv("SHELL")
+    };
+    for(int i = 0; i < 3; i++){
+        printf("%s: %s\n", sexium[i], envs[i]);
+    }
+    printf("\nCurrent process id: %d\n\n", getpid());
 }
 
-int main(){
-    int pid = getpid();
+int main(){ 
     char * banner = "       __\n" "    .-'  |\n" 
-                    "   /   <\\|\n" "  /     \\'\n" 
+                    "   /   <\\|\n" "  /     \\'\n"      //very smart logo
                     "  |_.- o-o\n" "  / C  -._)\\\n"
                     " /',        |\n" "|   `-,_,__,'\n"
                     "(,,)====[_]=|\n" "  '.   ____/\n"
                     "   | -|-|_\n" "   |____)_)";
-
-    printf("%s\n\n", banner);
-    system("cat /etc/issue");    //if it's a Linux distribution, returns the name
+    printf("\r%s\n\n", banner);  
     get_info();
-    const char * sexium[]={
-        "",                     
-        getenv("USER"),        //get enviroment variables
-        getenv("SHELL"),
-    };
-    for (int i=1; i < 3; i++){     
-        if (i == 1){
-            printf("User: %s\n", sexium[i]);    //Spaghetti code time
-        }else{
-            printf("Shell: %s\n\n", sexium[i]);
-        }   
-    }
-    printf("Current process id: %d\n\n", pid);
     return 0;
 }
