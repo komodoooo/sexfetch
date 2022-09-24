027688e4bab9013e35ccb34f527c42afd2972dcc
Komodo <loja@duck.com>
2022-09-24 14:40:12 +0200

    Update sexfetch.c

--- sexfetch.c
+++ sexfetch.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/utsname.h>
+#define RESET "\e[0m"       //reset colors
 
 void get_info(){
     system("cat /etc/issue");  //if it's a Linux distribution, returns the name
     struct utsname s;
     uname(&s);
-    printf("System: %s, %s\n",s.sysname,s.version);
+    printf("System: %s\nVersion: %s\n",s.sysname,s.version);
     printf("Kernel: %s\nCpu Architecture: %s\n",s.release,s.machine);
     printf("Hostname: %s\n",s.nodename);
     system("echo Ram: $(vmstat | awk 'END {printf $4}') Kb"); //get the ram
@@ -25,15 +26,16 @@ void get_info(){
         printf("%s: %s\n", sexium[i], envs[i]);
     }
     printf("\nCurrent process id: %d\n\n", getpid());
+    RESET;
 }
 
-int main(){ 
-    printf("\r       __\n" "    .-'  |\n" 
-        "   /   <\\|\n" "  /     \\'\n"      
-        "  |_.- o-o\n" "  / C  -._)\\\n"        //very smart logo
-        " /',        |\n" "|   `-,_,__,'\n"
-        "(,,)====[_]=|\n" "  '.   ____/\n"
-        "   | -|-|_\n" "   |____)_)\n\n");
+int main(){
+    printf("\e[1;93m" "\r\n       ⣿⣿\n"
+        "⠀⠀⠀⠀⠀⢀⣀⣿⣿⣀⡀\n""⠀⠀⠀⠀⠀⠈⣹⠟⠻⣏⠁\n"          //very smart logo
+    "⠀⠀⠀⠀⠀⣼⣯⣴⣦⣽⣧\n""⢠⣤⠀⢀⣼⡟⠁⢰⡆⠈⢻⣧⡀⠀⣤⡄\n"
+    "⠸⠿⢿⡿⠋⠻⢦⣌⣡⡴⠟⠙⢿⡿⠿⠇\n""⠀⢀⣿⣁⣀⣀⣀⣉⣉⣀⣀⣀⣈⣿⡀\n"
+    "⠀⠉⠉⠉⠉⣿⠉⠉⠉⠉⢹⡏⠉⠉⠉\n""⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠈⠉⠁\n"
+    RESET "\e[1;96m");
     get_info();
     return 0;
 }
